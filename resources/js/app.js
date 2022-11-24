3db5ad935c5b6dd1690151a437563d2469e5b37a
Agustina255 <paulaagustina25hotmail.com>
2022-11-24 13:01:50 -0300

    componentes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import React, {Component} from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {render} from 'react-dom'
+
+class MiApp extends Component {
+ render() {
+  return (
+   <Header/>
+    <h1> La mejor App del mundo </h1> 
+   <Footer year="2017"/>
+  )
+ }
+}
+
+render(<MiApp/>); document.querySelector('#app');
